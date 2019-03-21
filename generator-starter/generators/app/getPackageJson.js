affa0474d38833dd209fb7662720f96f53ca0859
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2019-03-21 08:58:19 +0200

    Update getting config for htmllint

--- generator-starter/generators/app/getPackageJson.js
+++ generator-starter/generators/app/getPackageJson.js
@@ -12,10 +12,11 @@ module.exports = () => {
     license: "ISC",
 
     scripts: {
-      "install": "npm run copy:editor-config",
+      "install": "npm run copy:editor-config && npm run copy:htmllint-config",
       "copy:editor-config": "cp node_modules/@mate-academy/editor-config/.editorconfig ./.editorconfig",
+      "copy:htmllint-config": "cp node_modules/@mate-academy/htmllint-config/.htmllintrc ./.htmllintrc",
       "validate:html": "foreach -g ./src/**/*.html --no-c -x \"html-validator --file #{path} --format=gnu --verbose\"",
-      "lint:html": "htmllint ./src/**/*.html --config node_modules/@mate-academy/htmllint-config/.htmllintrc",
+      "lint:html": "htmllint ./src/**/*.html",
       "lint:css": "stylelint ./src/**/*.css",
       "lint:js": "eslint ./src/**/*.js",
       "start": "browser-sync start --config browser-sync.config.js",
@@ -24,18 +25,18 @@ module.exports = () => {
     },
 
     devDependencies: {
-      "husky": "^1.3.1",
-      "browser-sync": "^2.26.3",
-      "eslint": "^5.13.0",
-      "htmllint-cli": "0.0.7",
-      "lighthouse": "^4.1.0",
-      "stylelint": "^9.10.1",
-      "foreach-cli": "^1.8.1",
-      "html-validator-cli": "^5.0.0",
       "@mate-academy/stylelint-config": "latest",
       "@mate-academy/htmllint-config": "latest",
       "@mate-academy/editor-config": "latest",
-      "@mate-academy/eslint-config": "latest"
+      "@mate-academy/eslint-config": "latest",
+      "browser-sync": "^2.26.3",
+      "eslint": "^5.13.0",
+      "foreach-cli": "^1.8.1",
+      "html-validator-cli": "^5.0.0",
+      "htmllint-cli": "0.0.7",
+      "husky": "^1.3.1",
+      "lighthouse": "^4.1.0",
+      "stylelint": "^9.10.1",
     },
 
     "husky": {
