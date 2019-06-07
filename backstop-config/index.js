dda2837cd2fbf2a09648902ee6ad5e7a6053430d
gestap <gestap9@gmail.com>
2019-06-07 12:46:09 +0300

    added backstop-config

---
+++ backstop-config/index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  basicScenario: {
+    label: 'test', // name of the test
+    url: 'http://localhost:8080/index.html',
+    referenceUrl: '', // put here reference to github-pages with ready project
+    readyEvent: '',
+    readySelector: '',
+    delay: 1000,
+    hideSelectors: ['[data-qa-hidden]'],
+    removeSelectors: ['[data-qa-remove]'],
+    hoverSelector: '',
+    clickSelector: '',
+    postInteractionWait: 0,
+    selectors: '',
+    selectorExpansion: true,
+    expect: 0,
+    misMatchThreshold: 1, // 1% of 100%
+  },
+  id: 'test',
+  onBeforeScript: 'puppet/onBefore.js',
+  onReadyScript: 'puppet/onReady.js',
+  viewports: [
+    {
+      name: 'tablet_h',
+      width: 1024,
+      height: 768,
+    },
+  ],
+  scenarios: [
+    { ...basicScenario },
+    // define here scenarios for testing
+  ],
+  paths: {
+    bitmaps_reference: 'backstop_data/bitmaps_reference',
+    bitmaps_test: 'backstop_data/bitmaps_test',
+    engine_scripts: 'backstop_data/engine_scripts',
+    html_report: 'backstop_data/html_report',
+    ci_report: 'backstop_data/ci_report',
+  },
+  report: ['browser'],
+  engine: 'puppeteer',
+  engineOptions: {
+    args: ['--no-sandbox'],
+  },
+  asyncCaptureLimit: 5,
+  asyncCompareLimit: 50,
+  debug: false,
+  debugWindow: false,
+};
