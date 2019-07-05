34c423104e863687bda3ad5bdd63ee05596917e5
gestap <gestap9@gmail.com>
2019-07-05 16:15:34 +0300

    fixed whitespaces

--- eslint_react-config/index.js
+++ eslint_react-config/index.js
@@ -21,7 +21,6 @@ module.exports = {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
     "no-console": "error",
     "no-param-reassign": [2, {"props": true}],
-
     "no-shadow": ["error", { "builtinGlobals": false }],
     "react/destructuring-assignment": 0,
     "jsx-a11y/label-has-associated-control": [2, {
@@ -34,10 +33,10 @@ module.exports = {
     "padding-line-between-statements": [
       "error",
       {"blankLine": "always", "prev": "*", "next": "return"},
-      { "blankLine": "always", "prev": ["const", "let", "var"], "next": "*"},
-      { "blankLine": "any",    "prev": ["const", "let", "var"], "next": ["const", "let", "var"]},
-      { "blankLine": "always", "prev": "directive", "next": "*" },
-      { "blankLine": "always", "prev": "block-like", "next": "*" },
+      {"blankLine": "always", "prev": ["const", "let", "var"], "next": "*"},
+      {"blankLine": "any", "prev": ["const", "let", "var"], "next": ["const", "let", "var"]},
+      {"blankLine": "always", "prev": "directive", "next": "*"},
+      {"blankLine": "always", "prev": "block-like", "next": "*"},
     ]
   }
 };
