afbef6ec7e0e2c721658ec08cf2513a8bd3e0131
Yurii Holiuk <bionicuss@gmail.com>
2020-05-18 19:00:15 +0300

    fix

--- mate-scripts/src/migrations/0.0.0.ts
+++ mate-scripts/src/migrations/0.0.0.ts
@@ -2,26 +2,40 @@ import fs from 'fs-extra';
 import path from "path";
 import { execBashCodeSafely } from '../tools';
 
-export default (rootDir: string) => {
+function safeRun(cb: Function) {
   try {
+    cb();
+  } catch (error) {
+    console.error('Migration error', error);
+  }
+}
+
+export default (rootDir: string) => {
+  safeRun(() => (
     fs.copySync(
       path.join(rootDir, 'config/backstop/backstopConfig.js'),
       path.join(rootDir, 'backstopConfig.js'),
-    );
-    fs.removeSync(path.join(rootDir, 'config'));
-    fs.removeSync(path.join(rootDir, 'server.js'));
-    fs.removeSync(path.join(rootDir, '.travis.yml'));
+    )
+  ));
+  safeRun(() => (
+    fs.removeSync(path.join(rootDir, 'config'))
+  ));
+  safeRun(() => (
+    fs.removeSync(path.join(rootDir, 'server.js'))
+  ));
+  safeRun(() => (
+    fs.removeSync(path.join(rootDir, '.travis.yml'))
+  ));
 
+  safeRun(() => {
     if (fs.existsSync(path.join(rootDir, 'readme.md'))) {
       fs.moveSync(
         path.join(rootDir, 'readme.md'),
         path.join(rootDir, 'README.md'),
       );
     }
+  });
 
-    execBashCodeSafely('npm rm @mate-academy/browsersync-config htmllint htmllint-cli husky lint-staged rimraf');
-    execBashCodeSafely('npm i -D @linthtml/linthtml @linthtml/gulp-linthtml gulp gulp-autoprefixer gulp-clean gulp-eslint gulp-replace-path gulp-sass gulp-sourcemaps gulp-stylelint stylelint-scss');
-  } catch (error) {
-    console.error('Migration error', error);
-  }
+  execBashCodeSafely('npm rm @mate-academy/browsersync-config htmllint htmllint-cli husky lint-staged rimraf');
+  execBashCodeSafely('npm i -D @linthtml/linthtml @linthtml/gulp-linthtml gulp gulp-autoprefixer gulp-clean gulp-eslint gulp-replace-path gulp-sass gulp-sourcemaps gulp-stylelint stylelint-scss');
 }
