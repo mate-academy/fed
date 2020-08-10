2f3c092cf896432919de489c132cf243f928cb42
Volodymyr Kryvytskyy <vovakrivickiy34@gmail.com>
2020-08-10 20:08:32 +0300

    added eslint internal config

---
+++ eslint-config-internal/index.js
@@ -0,0 +1,73 @@
+"use strict";
+
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020
+  },
+  extends: [
+    "airbnb/base",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  plugins: [
+    "@typescript-eslint",
+  ],
+  rules: {
+    // JS
+    "prefer-const": "error",
+    "curly": ["error", "all"],
+    "no-redeclare": ["error", { "builtinGlobals": true }],
+    "operator-linebreak": ["error", "before"],
+    "brace-style": ["error", "1tbs"],
+    "arrow-body-style": "off",
+    "no-param-reassign": ["error", { "props": true }],
+    "padding-line-between-statements": [
+      "error",
+      {"blankLine": "always", "prev": "*", "next": "return"},
+      {"blankLine": "always", "prev": ["const", "let", "var"], "next": "*"},
+      {"blankLine": "any", "prev": ["const", "let", "var"], "next": ["const", "let", "var"]},
+      {"blankLine": "always", "prev": "directive", "next": "*"},
+      {"blankLine": "always", "prev": "block-like", "next": "*"},
+    ],
+    "no-underscore-dangle": ["error", { "allow": [ "__resolveType" ] }],
+    "no-useless-constructor": "off",
+    "global-require": "off",
+    "import/no-dynamic-require": "off",
+    "import/prefer-default-export": "off",
+    "import/extensions": "off",
+    "max-classes-per-file": "off",
+    "max-len": ["error", 80, {
+      "ignoreStrings": true,
+      "ignoreTemplateLiterals": true,
+      "ignoreRegExpLiterals": true,
+      "ignoreComments": true,
+    }],
+    "arrow-parens": ["error", "always"],
+    "quotes": ["error", "single", { "allowTemplateLiterals": true }],
+    "no-console": ["error"],
+    "semi": ["error", "always"],
+    "import/order": ["error", {
+      "groups": [
+        'builtin',
+        'external',
+        'internal',
+        ['parent', 'sibling'],
+        'index',
+      ],
+      'newlines-between': 'ignore',
+    }],
+
+    // typescript
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/camelcase": "off",
+    "@typescript-eslint/no-unused-vars": ["error"],
+    "@typescript-eslint/indent": ["error", 2],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-useless-constructor": "error",
+  }
+};
---
+++ eslint-config-internal/readme.md
@@ -0,0 +1,20 @@
+# @mate-academy/eslint-config-internal
+
+#### React Typescript eslint-config based on airbnb config [Read more](https://mate-academy.github.io/style-guides/javascript-standard-modified) about rules.
+
+
+## Install
+
+```bash
+npm install --save-dev @mate-academy/eslint-config-internal
+```
+
+## Usage
+
+Add this to your `.eslintrc` file:
+
+```
+{
+  "extends": "@mate-academy/eslint-config-internal"
+}
+```
