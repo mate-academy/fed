cdc451240c2dc684af31bc054e9eb789c729f2f9
YuriiHoliuk <yurii.holiuk@mate.academy>
2022-02-22 16:05:10 +0200

    Update script

--- mate-scripts/src/localScripts/redelieverFailedWebHooks/main.ts
+++ mate-scripts/src/localScripts/redelieverFailedWebHooks/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable no-await-in-loop,no-plusplus,no-console */
 import { Octokit } from '@octokit/rest';
 import dotenv from 'dotenv';
@@ -14,14 +15,14 @@ dotenv.config();
 const githubToken = process.env.GITHUB_TOKEN;
 const orgName = 'mate-academy';
 
-// const forkHookId = 254454565;
+const forkHookId = 254454565;
 const pullRequestHookId = 214711671;
 const pullRequestReviewHookId = 239729647;
 const workflowRunHookId = 329862186;
 const checkRunHookId = 329862261;
 
 const hookIds = [
-  // forkHookId,
+  forkHookId,
   pullRequestHookId,
   pullRequestReviewHookId,
   workflowRunHookId,
@@ -29,7 +30,7 @@ const hookIds = [
 ];
 
 const hookNames = {
-  // [forkHookId]: 'Fork',
+  [forkHookId]: 'Fork',
   [pullRequestHookId]: 'Pull Request',
   [pullRequestReviewHookId]: 'Pull Request review',
   [workflowRunHookId]: 'Workflow run',
@@ -48,131 +49,123 @@ const octokit = new Octokit({
   auth: githubToken,
 });
 
-const successTimestamp = new Date('2022-02-17T16:14:04Z').getTime();
-
 function getLast<T>(arr: T[]): T | undefined {
   return arr[arr.length - 1];
 }
 
-function toChunks<T>(arr: T[], size = 10): T[][] {
-  const chunks = [];
+class Redelivery {
+  private tillDate: Date;
 
-  for (let i = 0; i < arr.length; i += 1) {
-    const start = i;
-    const end = i + (size - 1) > arr.length - 1
-      ? arr.length - 1
-      : i + (size - 1);
-
-    const chunk: T[] = [];
-
-    chunks.push(chunk);
-
-    for (let j = start; j <= end; j += 1) {
-      chunk.push(arr[j]);
-    }
-
-    i = end;
+  constructor(
+    tillDate: string,
+  ) {
+    this.tillDate = new Date(tillDate);
   }
 
-  return chunks;
-}
+  async run() {
+    await hookIds.reduce(async (prevPromise, hookId) => {
+      await prevPromise;
+      const log = makeLog(hookId);
 
-async function getNextDeliveries(
-  hookId: number,
-  lastId?: number,
-): Promise<Delivery[]> {
-  const { data: deliveries } = await octokit.request(
-    lastId
-      ? 'GET /orgs/{orgName}/hooks/{hookId}/deliveries?per_page={perPage}&cursor=v1_{lastId}'
-      : 'GET /orgs/{orgName}/hooks/{hookId}/deliveries?per_page={perPage}',
-    {
+      log('START');
+
+      await this.redeliverHook(hookId);
+
+      log('FINISH');
+    }, Promise.resolve());
+  }
+
+  private async redeliverHook(hookId: number) {
+    const log = makeLog(hookId);
+    const allDeliveries = [];
+
+    let last: Delivery | undefined;
+    let fetchMore = true;
+    let count = 0;
+
+    do {
+      count++;
+
+      const currentDeliveries = await this.getNextDeliveries(
+        hookId,
+        last ? last.id : undefined,
+      );
+
+      log(`Fetched Deliveries ${count} time`);
+
+      last = getLast(currentDeliveries);
+
+      if (
+        !last
+        || (
+          new Date(last.delivered_at).getTime() < this.tillDate.getTime()
+        )
+      ) {
+        fetchMore = false;
+      }
+
+      const failedDeliveries = currentDeliveries.filter((delivery) => (
+        delivery.status_code !== 200
+      ));
+
+      allDeliveries.push(...failedDeliveries);
+    } while (fetchMore);
+
+    log(`Fetched ${allDeliveries.length} deliveries`);
+
+    await allDeliveries.reduceRight(async (prevPromise, delivery, index) => {
+      await prevPromise;
+
+      await this.redeliverDelivery(hookId, delivery.id);
+
+      lastProcessed = delivery.id;
+
+      if (index % 50) {
+        log(`${index + 1}/${allDeliveries.length} processed`);
+      }
+    }, Promise.resolve());
+  }
+
+  private redeliverDelivery(hookId: number, deliveryId: number) {
+    return octokit.request('POST /orgs/{orgName}/hooks/{hookId}/deliveries/{deliveryId}/attempts', {
       orgName,
       hookId,
-      perPage: 100,
-      lastId,
-    },
-  );
+      deliveryId,
+    });
+  }
 
-  return deliveries;
-}
-
-function redeliverDelivery(hookId: number, deliveryId: number) {
-  return octokit.request('POST /orgs/{orgName}/hooks/{hookId}/deliveries/{deliveryId}/attempts', {
-    orgName,
-    hookId,
-    deliveryId,
-  });
-}
-
-export async function redeliverHook(hookId: number) {
-  const log = makeLog(hookId);
-  const allDeliveries = [];
-
-  let last: Delivery | undefined;
-  let fetchMore = true;
-  let count = 0;
-
-  do {
-    count++;
-
-    const currentDeliveries = await getNextDeliveries(
-      hookId,
-      last ? last.id : undefined,
+  private async getNextDeliveries(
+    hookId: number,
+    lastId?: number,
+  ): Promise<Delivery[]> {
+    const { data: deliveries } = await octokit.request(
+      lastId
+        ? 'GET /orgs/{orgName}/hooks/{hookId}/deliveries?per_page={perPage}&cursor=v1_{lastId}'
+        : 'GET /orgs/{orgName}/hooks/{hookId}/deliveries?per_page={perPage}',
+      {
+        orgName,
+        hookId,
+        perPage: 100,
+        lastId,
+      },
     );
 
-    log(`Fetched Deliveries ${count} time`);
-
-    last = getLast(currentDeliveries);
-
-    if (
-      !last
-      || (
-        new Date(last.delivered_at).getTime() < successTimestamp
-      )
-    ) {
-      fetchMore = false;
-    }
-
-    const failedDeliveries = currentDeliveries.filter((delivery) => (
-      delivery.status_code === 500
-      && !delivery.redelivery
-    ));
-
-    allDeliveries.push(...failedDeliveries);
-  } while (fetchMore);
-
-  log(`Fetched ${allDeliveries.length} deliveries`);
-
-  const deliveriesChunks = toChunks(allDeliveries);
-
-  log(`${deliveriesChunks.length} chunks created`);
-
-  await deliveriesChunks.reduce(async (prevPromise, chunk, index) => {
-    await prevPromise;
-
-    await Promise.all(
-      chunk.map(async (delivery) => {
-        await redeliverDelivery(hookId, delivery.id);
-
-        lastProcessed = delivery.id;
-      }),
-    );
-
-    log(`Chunk ${index + 1}/${deliveriesChunks.length} processed`);
-  }, Promise.resolve());
+    return deliveries;
+  }
 }
 
-async function main() {
-  await hookIds.reduce(async (prevPromise, hookId) => {
-    await prevPromise;
-    const log = makeLog(hookId);
+function main() {
+  const tillDate = process.argv[2];
 
-    log('START');
+  if (!tillDate) {
+    throw new Error(
+      'Usage: npm run redeliver "ISODate"("2022-02-17T16:14:04Z")',
+    );
+  }
 
-    await redeliverHook(hookId);
+  const redelivery = new Redelivery(tillDate);
 
-    log('FINISH');
-  }, Promise.resolve());
+  return redelivery.run();
 }
 
 main()
