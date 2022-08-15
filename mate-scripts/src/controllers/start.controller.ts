660fb7c5319ad69a8b8fed759e9eb9f117b61dfe
Danylo Onishchuk <danylo.o@mate.academy>
2022-08-15 17:26:34 +0300

    update start srcipt

--- mate-scripts/src/controllers/start.controller.ts
+++ mate-scripts/src/controllers/start.controller.ts
@@ -2,10 +2,11 @@ import { StartOptions } from '../commands';
 import { Controller } from './Controller';
 
 export const startController: Controller<StartOptions> = (command) => {
-  const { logs, open } = command;
+  const { logs, open, port } = command;
 
   return {
     shouldShowInternalLogs: logs,
     open,
+    port,
   };
 };
