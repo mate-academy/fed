5bbb10e641cc5a0e5743596539b3421a7d338b9d
Misha Grinko <glukym@gmail.com>
2023-11-10 13:55:25 +0200

    fix depth (#1)
    Co-authored-by: Misha Hrynko <misha@mate.academy>

--- mate-scripts/.stylelintrc.js
+++ mate-scripts/.stylelintrc.js
@@ -1,8 +1,5 @@
 module.exports = {
-  extends: [
-    "stylelint-config-standard-scss",
-    "@mate-academy/stylelint-config"
-  ],
+  extends: "@mate-academy/stylelint-config",
   plugins: [
     "stylelint-scss"
   ],
--- stylelint-config/index.js
+++ stylelint-config/index.js
@@ -8,21 +8,18 @@ module.exports = {
     "stylelint-scss"
   ],
   rules: {
-    "number-max-precision": 2,
-
-    "unit-no-unknown": [true, {"ignoreUnits": ["fr"]}],
-
     "declaration-empty-line-before": ["always", {
       except: ["first-nested"],
       ignore: ["after-comment", "after-declaration", "inside-single-line-block"]
     }],
-
-    "selector-max-id": 1,
-    "selector-max-universal": 1,
-
-    "rule-empty-line-before": ["always", {ignore: ["after-comment", "first-nested", "inside-block"]}],
-
-    "media-feature-name-case": "lower",
-    "media-feature-range-notation": "prefix"
+    "function-url-quotes": null,
+    "media-feature-range-notation": "prefix",
+    "number-max-precision": 2,
+    "rule-empty-line-before": ["always", {
+      ignore: ["after-comment", "first-nested", "inside-block"]
+    }],
+    "selector-max-id": 0,
+    "selector-max-universal": 0,
+    "unit-no-unknown": [true, {"ignoreUnits": ["fr"]}],
   },
 };
